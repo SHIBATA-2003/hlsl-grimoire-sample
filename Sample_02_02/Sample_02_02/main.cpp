856b5ff565fc38079bedc712dd86d0cefceaae52
SHIBATA-2003 <109647273+SHIBATA-2003@users.noreply.github.com>
2024-02-05 02:27:10 +0900

    0205

--- Sample_02_02/Sample_02_02/main.cpp
+++ Sample_02_02/Sample_02_02/main.cpp
@@ -41,7 +41,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // 頂点配列を定義
     SimpleVertex vertices[] = {
         {
-            {-0.5f, -0.5f, 0.0f},
+            {-0.5f, 0.0f, 0.0f},
             { 1.0f, 0.0f, 0.0f }
         },
         {
@@ -49,9 +49,13 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
             { 0.0f, 1.0f, 0.0f }
         },
         {
-            { 0.5f, -0.5f, 0.0f },
+            { 0.5f, 0.0f, 0.0f },
             { 0.0f, 0.0f, 1.0f }
-        }
+        },
+        {
+            { 1.0f, 0.5f, 0.0f },
+            { 0.0f, 1.0f, 0.0f }
+        },
     };
 
     VertexBuffer triangleVB;
@@ -61,7 +65,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // 5. 三角形のインデックスバッファを作成
     //インデックス配列
     uint16_t indices[] = {
-        0,1,2
+        0,1,2,
+        2,3,1,
     };
     IndexBuffer triangleIB;
     triangleIB.Init(sizeof(indices), 2);
@@ -93,7 +98,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         // 5. インデックスバッファを設定
         renderContext.SetIndexBuffer(triangleIB);
         // 6. ドローコール
-        renderContext.DrawIndexed(3);
+        renderContext.DrawIndexed(_countof(indices));
 
         /// //////////////////////////////////////
         // 絵を描くコードを書くのはここまで！！！
